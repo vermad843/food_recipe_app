c32750c3273db93e4b96a0940ff84b3106cd7593
vermad843 <vishnuverma7524@gmail.com>
2021-03-14 01:54:46 +0530

    code structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React  from 'react';
+import './App.css';
+import Header from './components/Header';
+import Recipes from './components/Recipes';
+
+function App() {
+  return (
+    <div className="App">
+       <Header/>
+       <Recipes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Recipes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Recipes() {
+    return (
+        <div>
+           <h1>Recipes</h1>       
+        </div>
+    )
+}
+
+export default Recipes;
+
