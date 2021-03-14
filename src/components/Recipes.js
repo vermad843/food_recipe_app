aaeb5678a5542dd0ad823e7a899370ac59527b18
vermad843 <vishnuverma7524@gmail.com>
2021-03-14 08:24:08 +0530

    recipes list

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -7,7 +7,7 @@ const Recipes = (props) => {
            {
                recipes.map(recipe => (
                    <div className = "col-md-3 p-5">
-                      <div className = "card">
+                      <div className = "card py-2 text-center">
                          <img 
                            src = {recipe.recipe.image}
                            className = "img-fluid w-100 mx-auto rounded-circle"    
@@ -15,6 +15,12 @@ const Recipes = (props) => {
                          <div className = "card-body">
                              <h5>{recipe.recipe.label}</h5>
                          </div>
+                         <ul className = "list-group list-group-flush">
+                              {
+                                  recipe.recipe.ingredientLines.map(ingredient => (
+                                     <li className = "list-group-item">{ingredient}</li>
+                                  ))}
+                         </ul>
                       </div>
                    </div>
                ))
